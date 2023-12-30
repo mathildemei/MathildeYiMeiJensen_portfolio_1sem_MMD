1f1a596756362461252b6afba0acbfa0250e797f
Mathilde <maje0014@stud.kea.dk>
2023-12-30 16:15:40 +0100

    lavet scroll btn i index

---
+++ js/portfolioscript.js
@@ -0,0 +1,13 @@
+// // deklarerer constanten
+// const ToTop = document.querySelector("#scrollTop");
+
+// // lyt efter klik funktionen
+// document.querySelector("#totop").addEventListener("click", smoothScroll);
+
+// function smoothScroll() {
+
+//     document.querySelector("#totop").addEventListener("click", smoothScroll);
+
+//     window.scrollTo (0,0);
+    
+
